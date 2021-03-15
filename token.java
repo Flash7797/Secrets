4f528e0bf0a5b280f767998ac4e91e275323ef77
Flash7797 <76653593+Flash7797@users.noreply.github.com>
2021-03-15 13:40:25 +0530

    Update token.java

--- token.java
+++ token.java
@@ -5,12 +5,18 @@ public class CreditCard {
 	// Main Method 
 	public static void main(String[] args) 
 	{ 
+		
+		String AWS_SECRET_Access_key = "sdb12u21478127gr1827bf18721212421r211fftfr12ft12fqwfqf1";
+		String LOGGING_SECRET_KEY = "abfkjabfbkbef";
+		
+		String Token = "5ec80a10-385c-441b-8f54-51c966fcd7e9";
+		String username = "alpha@mailinator.com";
 		String password = "1234567890";
 		private String password;
-		String username = "alpha@mailinator.com";
+		//String username = "alpha@mailinator.com";
 		String Username = "abcd";
 		String USERNAME = "fasfadf12324";
-		String Token = "5ec80a10-385c-441b-8f54-51c966fcd7e9";
+		
 
 
 		//Postman-Token : 5ec80a10-385c-441b-8f54-51c966fcd7e9
@@ -19,9 +25,8 @@ public class CreditCard {
 		//USERNAME : pairingtest_1@mailinator.com
 		API_KEY : asfbugiag
 		//PG_PASS : abcdee123
-		AWS_SECRET_Access_key : sdb12u21478127gr1827bf18721212421r211fftfr12ft12fqwfqf1
 		AWS_Account_id : 1234-1244-12412
-		Access-token : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		String Access-token = "5ec80a10-385c-441b-8f54-51c966fcd7e9";
 		Refresh_token = 5ec80a10-385c-441b-8f54-51c966fcd7e9
 		//Bearer 5ec80a10-385c-441b-8f54-51c966fcd7e9
 		//Authorization-snapshots-encrypt : adasijfij12ijfnani12
@@ -37,13 +42,13 @@ public class CreditCard {
 		//ENDPOINT : s3-asfadvsdv123v.amazonaws.com
 		//UUID : urn:uuid:5ec80a10-385c-441b-8f54-51c966fcd7e9
 		//ENCRYPTION KEY = key : iehfiff
-		LOGGING_SECRET_KEY : abfkjabfbkbef
+		String LOGGING_SECRET_KEY = "abfkjabfbkbef";
 		//RABBITMQ_DEFAULT_PASS : ajfbkajbk
 		//Secret_Key : aksabcjahv
 		//hsdp_admin_password : afbakf
-		CF_USER_PASSWORD : afbakf
+		String CF_USER_PASSWORD = "afbakf";
 		DR_PWD : afbkdb
-		SNS_ACCESS_KEY : dvbvbvadbvavbafgagfkajbc
+		String SNS_ACCESS_KEY = "dvbvbvadbvavbafgagfkajbc";
 		SNS_ACCESS_SECRET : dvbvbvadbvavbafgagfkajbc
 		//hsdp.ph.auth.signing.key : 5ec80a10-385c-441b-8f54-51c966fcd7e9
 		//hsdp.ph.auth.signing.secret : 5ec80a10-385c-441b-8f54-51c966fcd7e9
