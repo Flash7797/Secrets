b3c91bceedb175f4947f82e86e6138d23ace6bbb
Flash7797 <76653593+Flash7797@users.noreply.github.com>
2021-03-15 12:55:43 +0530

    Add files via upload

---
+++ token.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner; 
+
+public class CreditCard { 
+	// Main Method 
+	public static void main(String[] args) 
+	{ 
+		String password = "1234567890";
+		private String password;
+		String username = "alpha@mailinator.com";
+		String Username = "abcd";
+		String USERNAME = "fasfadf12324";
+		String Postman-Token = "5ec80a10-385c-441b-8f54-51c966fcd7e9";
+
+
+		//Postman-Token : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//Postman_id : 5ec80a10-385c-441b-8f54-51c966fcd7e9	
+		//username = hsdpconnectadmin
+		//USERNAME : pairingtest_1@mailinator.com
+		//API_KEY : asfbugiag
+		//PG_PASS : abcdee123
+		//AWS_SECRET_Access_key : sdb12u21478127gr1827bf18721212421r211fftfr12ft12fqwfqf1
+		//AWS_Account_id : 1234-1244-12412
+		//Access-token : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//Refresh_token = 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//Bearer 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//Authorization-snapshots-encrypt : adasijfij12ijfnani12
+		//Client-Id : adasijfij12ijfnani12
+		//Client_Secret : asadaasfaf12fdg
+		//DB_NAME : asfit
+		//Secret_Id : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//HOST = redshift://abcd:acba
+		//Service_Secret_Path : /v2:/cf5ec80a10-385c-441b-8f54-51c966fcd7e9/secret
+		//Space_Secret_Path : /v2:/cf5ec80a10-385c-441b-8f54-51c966fcd7e9/transit
+		//Org_Secret_Path : /v2:/cf5ec80a10-385c-441b-8f54-51c966fcd7e9/service
+		//Service_Transit_Path : /v2:/cf5ec80a10-385c-441b-8f54-51c966fcd7e9/secret
+		//ENDPOINT : s3-asfadvsdv123v.amazonaws.com
+		//UUID : urn:uuid:5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//ENCRYPTION KEY = key : iehfiff
+		//LOGGING_SECRET_KEY : abfkjabfbkbef
+		//RABBITMQ_DEFAULT_PASS : ajfbkajbk
+		//Secret_Key : aksabcjahv
+		//hsdp_admin_password : afbakf
+		//CF_USER_PASSWORD : afbakf
+		//DR_PWD : afbkdb
+		//SNS_ACCESS_KEY : dvbvbvadbvavbafgagfkajbc
+		//SNS_ACCESS_SECRET : dvbvbvadbvavbafgagfkajbc
+		//hsdp.ph.auth.signing.key : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//hsdp.ph.auth.signing.secret : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//hsdp.ph.customer.signing.key : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//hsdp.ph.services.signing.secret : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+		//hsdp.ph.subscription.signing.key : 5ec80a10-385c-441b-8f54-51c966fcd7e9
+	} 
+
+	
